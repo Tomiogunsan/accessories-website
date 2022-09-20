8698c7cde9f7d85d05b4ae03bc2c4d72314daaea
tomi ogunsan <tomiafolayan@gmail.com>
2022-09-20 04:47:05 -0700

    first commit

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -14,13 +14,13 @@ const Gallery = () => {
             <p>Explore rare modern accessories from our top collections</p>
         </div>
 
-        <div className='grid md:grid-cols-3 md:grid-rows-none gap-2 md:gap-4  mx-auto max-w-[1240px] h-full'>
-        <img className='md:col-span-2 md:row-span-2 w-full h-full object-cover' src={gal} alt='/'/>
-        <img className='w-full h-full object-cover' src={galTwo} alt='/'/>
-        <img className='w-full h-full object-cover' src={galThree} alt='/'/>
-        <img className='w-full h-full object-cover' src={galFour} alt='/'/>
-        <img className='w-full h-full object-cover md:col-span-2 md:row-span-2' src={galSix} alt='/'/>
-        <img className='w-full h-full object-cover' src={galS} alt='/'/>
+        <div className='grid md:grid-cols-3 grid-rows-none gap-2 md:gap-4 rounded-md  mx-auto max-w-[1240px] h-full'>
+        <img className='md:col-span-2 md:row-span-2 w-full h-full rounded-md object-cover' src={gal} alt='/'/>
+        <img className='w-full h-full rounded-md object-cover' src={galTwo} alt='/'/>
+        <img className='w-full h-full rounded-md object-cover' src={galThree} alt='/'/>
+        <img className='w-full h-full rounded-md object-cover' src={galFour} alt='/'/>
+        <img className='w-full h-full rounded-md object-cover md:col-span-2 md:row-span-2' src={galSix} alt='/'/>
+        <img className='w-full h-full rounded-md object-cover' src={galS} alt='/'/>
       
 
         </div>
